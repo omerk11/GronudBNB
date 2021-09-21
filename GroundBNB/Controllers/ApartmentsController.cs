9e96121dc02bb3c86ff49b9488aed328531e390b
Omer Keren <omerkeren11@gmail.com>
2021-09-21 18:16:45 +0300

    Added Aparatments & Users controllers, edited SiteContext

---
+++ GroundBNB/Controllers/ApartmentsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GroundBNB.Data;
+using GroundBNB.Models;
+
+namespace GroundBNB.Controllers
+{
+    public class ApartmentsController : Controller
+    {
+        private readonly SiteContext _context;
+
+        public ApartmentsController(SiteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Apartments
+        public async Task<IActionResult> Index()
+        {
+            var siteContext = _context.Apartments.Include(a => a.ApartmentOwner);
+            return View(await siteContext.ToListAsync());
+        }
+
+        // GET: Apartments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var apartment = await _context.Apartments
+                .Include(a => a.ApartmentOwner)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+
+            return View(apartment);
+        }
+
+        // GET: Apartments/Create
+        public IActionResult Create()
+        {
+            ViewData["ApartmentOwnerID"] = new SelectList(_context.Users, "ID", "Email");
+            return View();
+        }
+
+        // POST: Apartments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Title,Description,NumOfRooms,PricePerDay,City,Street,Floor,ApartmentNumber,ApartmentOwnerID")] Apartment apartment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(apartment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApartmentOwnerID"] = new SelectList(_context.Users, "ID", "Email", apartment.ApartmentOwnerID);
+            return View(apartment);
+        }
+
+        // GET: Apartments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var apartment = await _context.Apartments.FindAsync(id);
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+            ViewData["ApartmentOwnerID"] = new SelectList(_context.Users, "ID", "Email", apartment.ApartmentOwnerID);
+            return View(apartment);
+        }
+
+        // POST: Apartments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Title,Description,NumOfRooms,PricePerDay,City,Street,Floor,ApartmentNumber,ApartmentOwnerID")] Apartment apartment)
+        {
+            if (id != apartment.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(apartment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApartmentExists(apartment.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApartmentOwnerID"] = new SelectList(_context.Users, "ID", "Email", apartment.ApartmentOwnerID);
+            return View(apartment);
+        }
+
+        // GET: Apartments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var apartment = await _context.Apartments
+                .Include(a => a.ApartmentOwner)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+
+            return View(apartment);
+        }
+
+        // POST: Apartments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var apartment = await _context.Apartments.FindAsync(id);
+            _context.Apartments.Remove(apartment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ApartmentExists(int id)
+        {
+            return _context.Apartments.Any(e => e.ID == id);
+        }
+    }
+}
