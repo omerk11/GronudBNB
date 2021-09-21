e585168a034ab4fcbbf5c8589a919e81c0e59e3d
Ido Rosenberger <Idoldo217@gmail.com>
2021-09-21 13:43:07 +0300

    error last commit5

--- GroundBNB/Models/User.cs
+++ GroundBNB/Models/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,27 @@ namespace GroundBNB.Models
 {
     public class User
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required(ErrorMessage = "ID number required")]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "First Name required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name required")]
+        public string LastName { get; set; }
+        [Range(18,120)]
+        [Required(ErrorMessage = "Age required")]
+        public int Age { get; set; }
+        [Required(ErrorMessage = "Phone Number required")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Email address required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "The password is required")]
+        public string Password { get; set; }
+        public ICollection<Apartment> Apartments { get; set; }
+        public ICollection<Reservation> Reservations { get; set; }
+        public bool IsAdmin { get; set; }
+
+
     }
 }
