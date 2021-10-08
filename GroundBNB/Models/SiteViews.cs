39a3c1553aabd95908c51be68f15a8e6ae0889da
Omer Keren <omerkeren11@gmail.com>
2021-10-08 18:10:08 +0300

    Added Statistics services

---
+++ GroundBNB/Models/SiteViews.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroundBNB.Models
+{
+    public class SiteViews
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime Date { get; set; }
+
+        public int Views { get; set; }
+    }
+}
---
+++ GroundBNB/Services/ApartmentViewsService.cs
@@ -0,0 +1,52 @@
+﻿using GroundBNB.Data;
+using GroundBNB.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroundBNB.Services
+{
+    public class ApartmentViewsService:IApartmentViewsService
+    {
+        private readonly SiteContext _context;
+
+        public ApartmentViewsService(SiteContext _context)
+        {
+            this._context = _context;
+        }
+
+        public void Increment(int id)
+        {
+            DateTime today = DateTime.Now.Date;
+            ApartmentViews entity = this._context.ApartmentViews.Where(x => x.Date.Date.Equals(today) && x.AparatmentID == id).FirstOrDefault();
+            if (entity != null)
+            {
+                entity.Views += 1;
+                this._context.Attach<ApartmentViews>(entity);
+                this._context.Entry(entity).State = EntityState.Modified;
+            }
+            else
+            {
+                entity = new ApartmentViews
+                {
+                    Views = 1,
+                    Date = today,
+                    AparatmentID = id
+                };
+                this._context.ApartmentViews.Add(entity);
+                this._context.Entry(entity).State = EntityState.Added;
+            }
+
+            this._context.SaveChanges();
+        }
+
+        public List<ApartmentViews> GetApartmentsViews(int id)
+        {
+            return this._context.ApartmentViews.Where(x => x.AparatmentID == id).ToList();
+        }
+
+    }
+
+}
